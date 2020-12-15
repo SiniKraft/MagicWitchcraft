5167697627b1253fc2b032b650d158bc0f272f47
natclaudix@gmail.com <natclaudix@gmail.com>
2020-12-15 14:17:54 +0100

    Fix bugs

--- src/main/java/fr/sinikraft/magicwitchcraft/MagicWitchcraftModElements.java
+++ src/main/java/fr/sinikraft/magicwitchcraft/MagicWitchcraftModElements.java
@@ -13,7 +13,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
@@ -65,7 +64,6 @@ public class MagicWitchcraftModElements {
 		}
 		Collections.sort(elements);
 		elements.forEach(MagicWitchcraftModElements.ModElement::initElements);
-		MinecraftForge.EVENT_BUS.register(new MagicWitchcraftModVariables(this));
 	}
 
 	public void registerSounds(RegistryEvent.Register<net.minecraft.util.SoundEvent> event) {
---
+++ src/main/java/fr/sinikraft/magicwitchcraft/potion/BlockFlyPotion.java
@@ -0,0 +1,110 @@
+
+package fr.sinikraft.magicwitchcraft.potion;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegistryEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.EffectType;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effect;
+import net.minecraft.entity.LivingEntity;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import fr.sinikraft.magicwitchcraft.procedures.MysteriousDimensionOnPotionActiveTickProcedure;
+import fr.sinikraft.magicwitchcraft.MagicWitchcraftModElements;
+
+@MagicWitchcraftModElements.ModElement.Tag
+public class BlockFlyPotion extends MagicWitchcraftModElements.ModElement {
+	@ObjectHolder("magic_witchcraft:blockfly")
+	public static final Effect potion = null;
+	@ObjectHolder("magic_witchcraft:blockfly")
+	public static final Potion potionType = null;
+	public BlockFlyPotion(MagicWitchcraftModElements instance) {
+		super(instance, 96);
+		FMLJavaModLoadingContext.get().getModEventBus().register(this);
+	}
+
+	@SubscribeEvent
+	public void registerEffect(RegistryEvent.Register<Effect> event) {
+		event.getRegistry().register(new EffectCustom());
+	}
+
+	@SubscribeEvent
+	public void registerPotion(RegistryEvent.Register<Potion> event) {
+		event.getRegistry().register(new PotionCustom());
+	}
+	public static class PotionCustom extends Potion {
+		public PotionCustom() {
+			super(new EffectInstance(potion, 600));
+			setRegistryName("blockfly");
+		}
+	}
+
+	public static class EffectCustom extends Effect {
+		private final ResourceLocation potionIcon;
+		public EffectCustom() {
+			super(EffectType.BENEFICIAL, -10066330);
+			setRegistryName("blockfly");
+			potionIcon = new ResourceLocation("magic_witchcraft:textures/blockfly.png");
+		}
+
+		@Override
+		public String getName() {
+			return "effect.blockfly";
+		}
+
+		@Override
+		public boolean isBeneficial() {
+			return true;
+		}
+
+		@Override
+		public boolean isInstant() {
+			return false;
+		}
+
+		@Override
+		public boolean shouldRenderInvText(EffectInstance effect) {
+			return true;
+		}
+
+		@Override
+		public boolean shouldRender(EffectInstance effect) {
+			return true;
+		}
+
+		@Override
+		public boolean shouldRenderHUD(EffectInstance effect) {
+			return true;
+		}
+
+		@Override
+		public void performEffect(LivingEntity entity, int amplifier) {
+			World world = entity.world;
+			double x = entity.posX;
+			double y = entity.posY;
+			double z = entity.posZ;
+			{
+				Map<String, Object> $_dependencies = new HashMap<>();
+				$_dependencies.put("entity", entity);
+				$_dependencies.put("x", x);
+				$_dependencies.put("y", y);
+				$_dependencies.put("z", z);
+				$_dependencies.put("world", world);
+				MysteriousDimensionOnPotionActiveTickProcedure.executeProcedure($_dependencies);
+			}
+		}
+
+		@Override
+		public boolean isReady(int duration, int amplifier) {
+			return true;
+		}
+	}
+}
